ce8bf8a09d9c126c426beeb8c77d94f11fce43c0
keonhyeonwoo <khw19980926@gmail.com>
2023-10-28 17:05:16 +0900

    Implement GET and POST operations for comments

--- src/controllers/comment/createCommentController.js
+++ src/controllers/comment/createCommentController.js
@@ -4,19 +4,20 @@ import Comment from "../../model/Comment"
 export const createCommentController = async (req, res) => {
     try {
         const {
-            body: { text, id, userId },
+            body: { text, id },
+            session: {
+                user: { _id }
+            }
         } = req;
-        // const sessionUserId = req.session.user._id;
         const post = await Post.findById(id).populate('comments');
-        const user = await User.findById(userId).populate('comments');
+        const user = await User.findById(_id).populate('comments');
         if (!post) {
             return res.status(404).json({ message: "Post not found." });
         }
-        console.log('user comment', user);
         const comment = await Comment.create({
             text,
             post: id,
-            author: userId
+            author: _id,
         });
         post.comments.push(comment);
         user.comments.push(comment);
--- src/controllers/comment/getCommentController.js
+++ src/controllers/comment/getCommentController.js
@@ -1,5 +1,6 @@
 import Comment from "../../model/Comment";
 export const getCommentController = async (req, res) => {
     const comment = await Comment.find().populate('post').populate('author');
+    console.log('comment', comment)
     return res.status(200).json(comment);
 }
